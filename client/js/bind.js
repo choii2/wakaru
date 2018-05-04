a485c955dbe168e2faaba8615110034d950f81f4
miyatsuki <miyatsuki.shiku@gmail.com>
2018-05-04 19:16:46 +0900

    fix #5 投稿フォームの追加と投稿したポストが上に来るようにした

--- client/js/bind.js
+++ client/js/bind.js
@@ -7,28 +7,63 @@ var myModel = {
 
 var myViewModel = new Vue({
     el: '#my_view',
-    data: {posts: []},
+    data: {
+        posts: [],
+        new_post : {
+            message: "",
+            user_id: 0
+        }
+    },
     created: function(){
         this.fetchData()
+    },
+    computed: {
+        orderByID: function () {
+          return _.orderBy(this.posts, 'id').reverse()
+        }
     },
     methods: {
         fetchData(){
             self = this;
-            console.log(myViewModel)
 
             var request = window.superagent;
             url = "https://4ihvpi6t63.execute-api.ap-northeast-1.amazonaws.com/dev/post"
             request
                 .get(url)
-//                .send({user_id:0, text:"ほげ"})
                 .end(function(err, res){
                     console.log(res)
                     console.log(res.text);//レスポンス
-                    //レスポンスがJSONの場合 
+                    //レスポンスがJSONの場合
                     console.log(JSON.parse(res.body));//ここにparse済みのオブジェクトが入る
                     self.posts = JSON.parse(res.body)
             });
-    
+        },
+        post: function(event){
+            self = this;
+
+            var request = window.superagent;
+            url = "https://4ihvpi6t63.execute-api.ap-northeast-1.amazonaws.com/dev/post"
+
+            request
+            .post(url)
+            .send({user_id:self.new_post.user_id, text:self.message})
+            .end(function(err, res){
+                console.log(res)
+                console.log(res.text);//レスポンス
+                //レスポンスがJSONの場合
+                console.log(JSON.parse(res.body));//ここにparse済みのオブジェクトが入る
+                console.log(self.posts)
+
+                request
+                .get(url)
+                .end(function(err, res){
+                    console.log(res)
+                    console.log(res.text);//レスポンス
+                    //レスポンスがJSONの場合
+                    console.log(JSON.parse(res.body));//ここにparse済みのオブジェクトが入る
+                    self.posts = JSON.parse(res.body)});;
+                }
+            )
         }
     }
 });
